b154feb022584a83dad2effa55f9b1d1637afbea
nicsilva <nicole.allarice@gmail.com>
2014-11-10 13:00:10 -0500

    request email works

--- www/js/controllers/verify-email.js
+++ www/js/controllers/verify-email.js
@@ -6,11 +6,17 @@ app.controller('VerifyEmailController', ['$scope', '$http', '$state', 'Restangul
     $scope.authError = null;
     $scope.verificationSent = false;
     $scope.verifyEmail = function() {
+      $scope.success = false;
       $scope.authError = null;
       // Try to create
-      Restangular.all('account/resend').post("user", {email: $scope.user.email}).then(function(response) {
-        if ( response.data ) {
-          $scope.authError = response;
+      var api = Restangular.all('account/resend');
+
+
+      // POST /accounts
+      api.post({email_address: $scope.user.email}).then(function(response) {
+        if ( response.code == 200 ) {
+          $scope.success = true;
+          $scope.authError = 'Success!  Please check your email for further instructions.';
         }else{
           $scope.verificationSent = true;
         }
